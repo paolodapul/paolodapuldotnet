db85cfe1f0eee95bee60e1f8f82a67f16165f66e
Paolo Dapul <paolodapultech@gmail.com>
2021-08-24 22:27:47 +0800

    Component for displaying blog post content

---
+++ pages/blog/[slug].js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import marked from 'marked';
+import Link from 'next/link';
+
+export default function PostPage({ frontMatter: { title, date }, content }) {
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <div>Posted on {date}</div>
+        <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'));
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', `${slug}.md`),
+    'utf-8'
+  );
+
+  const { data: frontMatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontMatter,
+      slug,
+      content,
+    },
+  };
+}

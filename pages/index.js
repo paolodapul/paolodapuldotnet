954911f81182ea38a07dd131cab01112ce186d05
Paolo Dapul <paolodapultech@gmail.com>
2021-08-24 20:44:15 +0800

    Remove reference to styles

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Paolo Dapul | Musings and Learnings</title>
         <meta name="description" content="Blog by Paolo Dapul." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h2>Hello</h2>
     </div>
   );
 }

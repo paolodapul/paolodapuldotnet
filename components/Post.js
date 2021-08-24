203444e235ebe58abf0f779a13d6d5c99b891964
Paolo Dapul <paolodapultech@gmail.com>
2021-08-24 22:31:09 +0800

    Update HTML structure of Post component

--- components/Post.js
+++ components/Post.js
@@ -3,12 +3,13 @@ import Link from 'next/link';
 export default function Post({ post }) {
   return (
     <div>
-      <h2>{post.frontMatter.title}</h2>
-      <p>{post.frontMatter.excerpt}</p>
-
       <Link href={`/blog/${post.slug}`}>
-        <a href="#">Read more</a>
+        <a>
+          <h2>{post.frontMatter.title}</h2>
+        </a>
       </Link>
+      <div>{post.frontMatter.date}</div>
+      <div>{post.frontMatter.excerpt}</div>
     </div>
   );
 }

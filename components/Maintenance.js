5310f28669f89f2b880e7a3b21d6675004e30753
Paolo Dapul <paolodapultech@gmail.com>
2022-05-28 14:49:43 +0800

    Maintenance mode

---
+++ components/Maintenance.js
@@ -0,0 +1,38 @@
+export default function Maintenance() {
+  return (
+    <article className="prose text-slate-700 font-heebo_regular container my-48 mx-auto lg:w-2/5 2xl:w-2/5">
+      <p className="mb-2">
+        I have an ongoing redesign and re-development of this site, but this
+        will be back eventually. Something better will be up :)
+      </p>
+      <p className="mb-2">
+        Currently busy building a full stack side project, and I&apos;ll be back
+        to writing more posts regularly after that.
+      </p>
+      <p className="mb-2">
+        For correspondence, feel free to reach my on{' '}
+        <a
+          href="https://www.linkedin.com/in/paolodapul/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        ,{' '}
+        <a
+          href="https://twitter.com/paolodapul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>{' '}
+        or email -{' '}
+        <a href="mailto:paolodapultech@gmail.com">paolodapultech@gmail.com</a>.
+      </p>
+      <p className="mb-2">
+        Best, <br />
+        Paolo
+      </p>
+    </article>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import Header from '@components/Header';
 import '../styles/globals.css'
+import Maintenance from '@components/Maintenance';
 
 function MyApp({ Component, pageProps }) {
+
+  if (process.env.MAINTENANCE === 'true') {
+    return <Maintenance />
+  } 
+
   return (
     <div className='container my-16 mx-auto lg:w-2/5 2xl:w-2/5'>
       <Header className="font-sans" />

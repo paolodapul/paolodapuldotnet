ad090cdf2a8240c66fdcbf988d68421725f5747f
Paolo Dapul <paolodapultech@gmail.com>
2022-09-04 01:57:23 +0800

    Fix responsiveness

--- components/Maintenance.js
+++ components/Maintenance.js
@@ -1,6 +1,6 @@
 export default function Maintenance() {
   return (
-    <article className="prose text-slate-700 font-inter_regular container px-10 my-48 md:mx-auto lg:mx-auto sm:w-2/5 lg:w-2/5 2xl:w-2/5 text-lg subpixel-antialiased">
+    <article className="prose text-slate-700 font-inter_regular container px-10 my-48 md:mx-auto lg:mx-auto sm:w-3/5 sm:mx-auto lg:w-2/5 2xl:w-2/5 text-lg subpixel-antialiased">
       <p className="mb-2">
         I have an ongoing revamp of this site, but this
         will be back eventually. Something better will be up. 👆🏼

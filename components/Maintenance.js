6dc2a273d218312cd25203066d336e64341a4643
Paolo Dapul <paolodapultech@gmail.com>
2022-09-23 20:51:53 +0800

    Add head component to maintenance page

--- components/Maintenance.js
+++ components/Maintenance.js
@@ -1,25 +1,37 @@
+import Head from 'next/head'
+
 export default function Maintenance() {
   return (
-    <article className="prose text-slate-700 font-inter_regular container px-10 my-48 md:mx-auto lg:mx-auto sm:w-3/5 sm:mx-auto lg:w-2/5 2xl:w-2/5 text-lg subpixel-antialiased">
-      <p className="mb-2">
-        I have an ongoing revamp of this site, but this
-        will be back eventually. Something better will be up. 👆🏼
-      </p>
-      <p className="mb-2">
-        For correspondence, feel free reaching out on{' '}
-        <a
-          href="https://www.linkedin.com/in/paolodapul/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        , or <a href="mailto:paolodapultech@gmail.com">email</a>.
-      </p>
-      <p className="mb-2">
-        Best, <br />
-        Paolo
-      </p>
-    </article>
+    <>
+      <Head>
+        <title>Paolo Dapul</title>
+        <meta
+          name="description"
+          content="I write about programming and software development."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <article className="prose text-slate-700 font-inter_regular container px-10 my-48 md:mx-auto lg:mx-auto sm:w-3/5 sm:mx-auto lg:w-2/5 2xl:w-2/5 text-lg subpixel-antialiased">
+        <p className="mb-2">
+          I have an ongoing revamp of this site, but this will be back
+          eventually. Something better will be up. 👆🏼
+        </p>
+        <p className="mb-2">
+          For correspondence, feel free reaching out on{' '}
+          <a
+            href="https://www.linkedin.com/in/paolodapul/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          , or <a href="mailto:paolodapultech@gmail.com">email</a>.
+        </p>
+        <p className="mb-2">
+          Best, <br />
+          Paolo
+        </p>
+      </article>
+    </>
   )
 }

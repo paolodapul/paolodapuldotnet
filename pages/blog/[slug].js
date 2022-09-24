ea0febf13be0517b18a4997f7408373d859e5d33
Paolo Dapul <paolodapultech@gmail.com>
2022-09-24 10:18:43 +0800

    Fixed error on marked package

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import marked from 'marked';
-import Link from 'next/link';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { marked } from 'marked'
+import Link from 'next/link'
 
 export default function PostPage({ frontMatter: { title, date }, content }) {
   return (
@@ -13,30 +13,30 @@ export default function PostPage({ frontMatter: { title, date }, content }) {
         <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
       </div>
     </>
-  );
+  )
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts'));
+  const files = fs.readdirSync(path.join('posts'))
   const paths = files.map((filename) => ({
     params: {
       slug: filename.replace('.md', ''),
     },
-  }));
+  }))
 
   return {
     paths,
     fallback: false,
-  };
+  }
 }
 
 export async function getStaticProps({ params: { slug } }) {
   const markdownWithMeta = fs.readFileSync(
     path.join('posts', `${slug}.md`),
     'utf-8'
-  );
+  )
 
-  const { data: frontMatter, content } = matter(markdownWithMeta);
+  const { data: frontMatter, content } = matter(markdownWithMeta)
 
   return {
     props: {
@@ -44,5 +44,5 @@ export async function getStaticProps({ params: { slug } }) {
       slug,
       content,
     },
-  };
+  }
 }

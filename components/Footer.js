d50e3c3f514b3e1976b5478abc48781cd3c218a8
Paolo Dapul <paolodapultech@gmail.com>
2022-10-14 12:25:31 +0800

    Update colors

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-neutral-900 h-28 flex items-center justify-center text-stone-300 font-inter_lights font-light text-xs">
+    <footer className="bg-black h-28 flex items-center justify-center text-stone-300 font-inter_lights font-light text-xs">
       &copy; {new Date().getFullYear()} Paolo Dapul, all rights reserved.
     </footer>
   )

2c61c6821e95f8376fb3a4c9b81bcb9805e05c57
Paolo Dapul <paolodapultech@gmail.com>
2023-10-01 16:05:48 +0800

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,10 @@ export default function About() {
     <div className="prose prose-invert">
       <h1 className="text-5xl text-stone-100">{`Hi, I'm Paolo.`}</h1>
       <hr />
-      <p>{`I write a lot of my thoughts down all the time, usually just to myself, but here, I'm going to dump my knowledge on things I learned, showing it to the world to make it useful (hopefully) to my fellow developers and enthusiasts. Being able to freely share and consume information over the web is one of the nicest things I like about life, because this fuels our joy of learning.`}</p>
-      <p>{`I'm a software developer, working usually on web stuff. I'm very much of a generalist when it comes to technologies, working with a variety of tech stacks throughout the span of my existence, but I prefer TypeScript, React and Node.js these days.`}</p>
+      <p>{`I write down a lot of my thoughts all the time, usually just for myself, but here, I'm going to share my knowledge on the things I've learned, showcasing it to the world to hopefully make it useful for my fellow developers and enthusiasts. Being able to freely share and consume information over the web is one of the things I love about life because it fuels our joy of learning.`}</p>
+      <p>{`I'm a software developer, primarily working on web-related projects. I consider myself a generalist when it comes to technologies, having worked with a variety of tech stacks throughout my existence.`}</p>
       <p>
-        {`If you'd like to know more about what I've been doing with my work life and to see how I look like, you can check my`}{' '}
+        {`If you'd like to know more about my work and see what I look like, you can check out my`}{' '}
         <a
           className="no-underline text-sky-300"
           target="_blank"
@@ -15,7 +15,7 @@ export default function About() {
         >
           LinkedIn profile
         </a>{' '}
-        {`- it's pretty updated. If you'd like to see how I write code, here is my `}
+        {`- it's pretty updated. If you're interested in seeing how I write code, you can find my `}
         <a
           className="no-underline text-sky-300"
           target="_blank"
@@ -24,14 +24,14 @@ export default function About() {
         >
           GitHub profile
         </a>
-        .
+        {` here.`}
       </p>
       <div className="text-right mt-24">
         <em>
-          Last updated: {new Date('9/24/2022').toLocaleDateString('en-PH')}
+          Last updated: {new Date('10/01/2023').toLocaleDateString('en-PH')}
         </em>
       </div>
       <hr />
     </div>
-  )
+  );
 }

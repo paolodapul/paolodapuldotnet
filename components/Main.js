8c47f3208f8315aca1e740b4c6756ccb084f3fa3
Paolo Dapul <paolodapultech@gmail.com>
2023-10-09 11:08:47 +0800

    Widen container

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 export default function Main({ children }) {
   return (
-    <main className="antialiased container px-8 lg:px-auto mx-auto lg:w-2/5 2xl:w-2/5 py-20 h-min">
+    <main className="antialiased container px-8 lg:px-auto mx-auto lg:w-6/12 2xl:w-6/12 py-20 h-min">
       {children}
     </main>
   );

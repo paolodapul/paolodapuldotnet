9488b0286e83e4d9086faccc6310ba2f741d8de1
Paolo Dapul <paolodapultech@gmail.com>
2023-10-30 20:54:07 +0800

    Fix next config for Next 14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   env: {
     MAINTENANCE: process.env.MAINTENANCE,
   },
-}
+};
+
+module.exports = nextConfig;
